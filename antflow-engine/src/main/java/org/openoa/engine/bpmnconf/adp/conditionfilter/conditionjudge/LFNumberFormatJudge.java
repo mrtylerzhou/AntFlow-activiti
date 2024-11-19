4b89476aaca5c3cef974a6892b6f93b90ab9dabe
TylerZhou <zypqqgc@qq.com>
2024-11-19 21:30:47 +0800

    *)condition judge big decimal add scale

--- antflow-engine/src/main/java/org/openoa/engine/bpmnconf/adp/conditionfilter/conditionjudge/LFNumberFormatJudge.java
+++ antflow-engine/src/main/java/org/openoa/engine/bpmnconf/adp/conditionfilter/conditionjudge/LFNumberFormatJudge.java
@@ -1,22 +1,20 @@
 package org.openoa.engine.bpmnconf.adp.conditionfilter.conditionjudge;
 
-import com.google.common.base.Strings;
 import lombok.extern.slf4j.Slf4j;
-import org.openoa.base.exception.JiMuBizException;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.openoa.base.vo.BpmnNodeConditionsConfBaseVo;
 import org.openoa.base.vo.BpmnStartConditionsVo;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
-import org.springframework.util.ObjectUtils;
 
-import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Service
 @Slf4j
 public class LFNumberFormatJudge extends AbstractLFConditionJudge{
     @Override
     public boolean judge(String nodeId, BpmnNodeConditionsConfBaseVo conditionsConf, BpmnStartConditionsVo bpmnStartConditionsVo) {
-       return super.lfCommonJudge(conditionsConf,bpmnStartConditionsVo,(a,b)->super.compareJudge(new BigDecimal(a.toString()),new BigDecimal(b.toString()),conditionsConf.getNumberOperator()));
+       return super.lfCommonJudge(conditionsConf,bpmnStartConditionsVo,(a,b)->super.compareJudge(NumberUtils.toScaledBigDecimal(a.toString(),2, RoundingMode.HALF_UP)
+               ,NumberUtils.toScaledBigDecimal(a.toString(),2, RoundingMode.HALF_UP),conditionsConf.getNumberOperator()));
     }
 
 }
