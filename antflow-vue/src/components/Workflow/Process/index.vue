9396eaa8e88f4a747afd09a41c5d3d27e0af01bf
lidonghui <574427343@qq.com>
2025-02-14 09:59:13 +0800

    流程设计页面滚动缩放，拖拽效果

--- antflow-vue/src/components/Workflow/Process/index.vue
+++ antflow-vue/src/components/Workflow/Process/index.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="app-container">
-        <section class="dingflow-design">
+        <section class="dingflow-design" ref="dingflowDesignRef">
             <div class="zoom">
-                <div class="zoom-out" :class="nowVal == 50 && 'disabled'" @click="zoomSize(1)"></div>
+                <div class="zoom-out" @click="zoomOut"></div>
                 <span>{{ nowVal }}%</span>
-                <div class="zoom-in" :class="nowVal == 300 && 'disabled'" @click="zoomSize(2)"></div>
+                <div class="zoom-in" @click="zoomIn"></div>
             </div>
-            <div class="box-scale" :style="`transform: scale(${nowVal / 100});`">
+            <div class="box-scale" ref="boxScaleRef">
                 <nodeWrap v-model:nodeConfig="nodeConfig" />
                 <div class="end-node">
                     <div class="end-node-circle"></div>
@@ -32,6 +32,7 @@ import promoterDrawer from "@/components/Workflow/drawer/promoterDrawer.vue";
 import approverDrawer from "@/components/Workflow/drawer/approverDrawer.vue";
 import copyerDrawer from "@/components/Workflow/drawer/copyerDrawer.vue";
 import conditionDrawer from "@/components/Workflow/drawer/conditionDrawer.vue";
+import {wheelZoomFunc, zoomInit} from "@/utils/zoom.js";
 const { proxy } = getCurrentInstance();
 let { setIsTried } = useStore()
 const emit = defineEmits(['nextChange'])
@@ -41,13 +42,17 @@ let props = defineProps({
         default: () => (null),
     }
 });
-
+const dingflowDesignRef = ref(null);
+const boxScaleRef = ref(null);
 let tipList = ref([]);
 let tipVisible = ref(false);
 let nowVal = ref(100);
 let nodeConfig = ref({});
 let directorMaxLevel = ref(3);
-onMounted(async () => {
+onMounted(async () => { 
+    zoomInit(dingflowDesignRef, boxScaleRef, (val) => { 
+        nowVal.value = val
+    })
     if (props.processData) {
         nodeConfig.value = props.processData;
     }
@@ -101,20 +106,28 @@ const reErr = ({ childNode }) => {
         childNode = null;
     }
 };
+/** 页面放大 */
+function zoomIn() {
+  wheelZoomFunc({scaleFactor: parseInt(nowVal.value) / 100 + 0.1, isExternalCall: true})
+}
 
-const zoomSize = (type) => {
-    if (type == 1) {
-        if (nowVal.value == 50) {
-            return;
-        }
-        nowVal.value -= 10;
-    } else {
-        if (nowVal.value == 300) {
-            return;
-        }
-        nowVal.value += 10;
-    }
-};
+/** 页面缩小 */
+function zoomOut() {
+  wheelZoomFunc({scaleFactor: parseInt(nowVal.value) / 100 - 0.1, isExternalCall: true})
+}
+// const zoomSize = (type) => {
+//     if (type == 1) {
+//         if (nowVal.value == 50) {
+//             return;
+//         }
+//         nowVal.value -= 10;
+//     } else {
+//         if (nowVal.value == 300) {
+//             return;
+//         }
+//         nowVal.value += 10;
+//     }
+// };
 
 const getJson = () => {
     setIsTried(true); 
