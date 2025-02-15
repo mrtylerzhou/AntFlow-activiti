5ba9c60bf0696bbedf7000a80652bd4183683911
TylerZhou <zypqqgc@qq.com>
2025-02-15 18:16:12 +0800

    *)流程委托对外部传入流程暂不生效

--- antflow-engine/src/main/java/org/openoa/engine/bpmnconf/activitilistener/BpmnTaskListener.java
+++ antflow-engine/src/main/java/org/openoa/engine/bpmnconf/activitilistener/BpmnTaskListener.java
@@ -90,6 +90,11 @@ public class BpmnTaskListener implements TaskListener {
         BpmnConf bpmnConf = bpmnConfService.getBaseMapper().selectOne(new QueryWrapper<BpmnConf>()
                 .eq("bpmn_code", bpmnCode));
 
+        if (bpmnConf==null) {
+            log.error("Task监听-查询流程配置数据为空，流程编号{}", processNumber);
+            throw new JiMuBizException("Task监听-查询流程配置数据为空");
+        }
+        boolean isOutside = Optional.ofNullable(bpmnConf.getIsOutSideProcess()).orElse(0).equals(1);
         //set process entrust info
         String oldUserId = delegateTask.getAssignee();
         String oldUserName="";
@@ -106,8 +111,9 @@ public class BpmnTaskListener implements TaskListener {
         }
 
 
+
         //如果委托生效 则在我的委托列表中加一条数据
-        if (!oldUserId.equals(userId)) {
+        if (!isOutside&&!oldUserId.equals(userId)) {
             BpmFlowrunEntrust entrust = new BpmFlowrunEntrust();
             entrust.setType(1);
             entrust.setRuntaskid(delegateTask.getId());
@@ -137,13 +143,6 @@ public class BpmnTaskListener implements TaskListener {
                 .build();
 
 
-        if (bpmnConf==null) {
-            log.error("Task监听-查询流程配置数据为空，流程编号{}", processNumber);
-            throw new JiMuBizException("Task监听-查询流程配置数据为空");
-        }
-
-
-        boolean isOutside = Optional.ofNullable(bpmnConf.getIsOutSideProcess()).orElse(0).equals(1);
 
 
         if (bpmVariableMessageListenerService.listenerCheckIsSendByTemplate(bpmVariableMessageVo)) {
