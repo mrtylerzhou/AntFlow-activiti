5cd670286b5bae5daef086421e50a054144348d8
lidonghui <574427343@qq.com>
2025-03-20 22:16:04 +0800

    跳转至若依

--- antflow-vue/src/components/Dashboard/Workplace.vue
+++ antflow-vue/src/components/Dashboard/Workplace.vue
@@ -72,6 +72,12 @@
                         <el-tooltip content="【*第三方流程（又称：业务方流程），外部系统的业务表单，需要审批流程，接入本流程引擎*】" placement="right">
                             <el-icon><question-filled /></el-icon>
                         </el-tooltip> 
+                        <el-tooltip content="更多体验三方接入，点击跳转若依管理系统" placement="right">
+                            <a href="http://117.72.70.166/ruoyi/#/hr/leavetime" target="_blank">
+                                <el-button type="success" plain icon="Guide">更多体验,点击跳转至若依管理系统</el-button>
+                            </a>
+                        </el-tooltip> 
+                   
                     </span>
                 </div>
             </template>
