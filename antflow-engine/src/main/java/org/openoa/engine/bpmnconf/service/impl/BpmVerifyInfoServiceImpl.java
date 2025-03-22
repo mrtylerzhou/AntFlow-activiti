9d7ae08b8d15fa52344182b4ce092b655e139b80
TylerZhou <zypqqgc@qq.com>
2025-03-22 16:56:12 +0800

    fix * fix node name dispaly problem when current tasks are in parallel running

--- antflow-engine/src/main/java/org/openoa/engine/bpmnconf/service/impl/BpmVerifyInfoServiceImpl.java
+++ antflow-engine/src/main/java/org/openoa/engine/bpmnconf/service/impl/BpmVerifyInfoServiceImpl.java
@@ -307,10 +307,10 @@ public class BpmVerifyInfoServiceImpl extends ServiceImpl<BpmVerifyInfoMapper, B
             String verifyUserName = StringUtils.join(tasks.stream().map(BpmVerifyInfoVo::getVerifyUserName).collect(Collectors.toList()), ",");
             String taskName = StringUtils.EMPTY;
             List<String> strs = tasks.stream().map(BpmVerifyInfoVo::getTaskName).filter(Objects::nonNull).collect(Collectors.toList());
-            if (!org.springframework.util.ObjectUtils.isEmpty(strs)) {
-                taskName = strs.get(0);
+            if (!CollectionUtils.isEmpty(strs)) {
+                taskName = String.join("||", strs);
             }
-            String elementId = tasks.stream().map(BpmVerifyInfoVo::getElementId).findFirst().orElse(StringUtils.EMPTY);
+            String elementId = tasks.stream().map(BpmVerifyInfoVo::getElementId).collect(Collectors.joining(","));
             taskInfors.add(BpmVerifyInfoVo.builder()
                             .verifyUserIds(verifyUserIds)
                     .verifyUserName(verifyUserName)
