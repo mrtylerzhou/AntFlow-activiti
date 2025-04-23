cfbb3819f04d768c7425ec3970cd67fb5e3dd274
TylerZhou <zypqqgc@qq.com>
2025-04-24 07:25:28 +0800

    opt 一处判断代码优化

--- antflow-engine/src/main/java/org/openoa/engine/bpmnconf/util/BpmnConfNodePropertyConverter.java
+++ antflow-engine/src/main/java/org/openoa/engine/bpmnconf/util/BpmnConfNodePropertyConverter.java
@@ -79,7 +79,7 @@ public class BpmnConfNodePropertyConverter {
                 String fixedDownBoxValue = newModel.getFixedDownBoxValue();
                 List<BaseKeyValueStruVo> valueStruVoList = JSON.parseArray(fixedDownBoxValue,BaseKeyValueStruVo.class);
                 String zdy1 = newModel.getZdy1();
-                if (zdy1.contains("[")&& zdy1.contains("]")){
+                if (zdy1.startsWith("[")&& zdy1.endsWith("]")){
                     zdy1 = zdy1.substring(1,zdy1.length() -1);
                 }
                 String[] keys = zdy1.split(",");
