5c4ad6ff0d10dba2a3baa19505ecb5d2cfde89a4
TylerZhou <zypqqgc@qq.com>
2025-04-29 21:15:38 +0800

    *)优化低代码分片判断逻辑

--- antflow-engine/src/main/java/org/openoa/engine/conf/mybatis/interceptor/LFConsistentHashingRoutingSqlInterceptor.java
+++ antflow-engine/src/main/java/org/openoa/engine/conf/mybatis/interceptor/LFConsistentHashingRoutingSqlInterceptor.java
@@ -155,7 +155,7 @@ public class LFConsistentHashingRoutingSqlInterceptor implements Interceptor, In
                 new DefaultReflectorFactory()
         );
 
-        try {
+        /*try {
             List<Map.Entry<String, String>> entries = BoundSqlUtils.extractWhereColumnsAndParams(boundSql);
             String idValue="";
             if(!CollectionUtils.isEmpty(entries)){
@@ -179,7 +179,7 @@ public class LFConsistentHashingRoutingSqlInterceptor implements Interceptor, In
                 BpmBusinessProcessServiceImpl businessProcessService = SpringBeanUtils.getBean(BpmBusinessProcessServiceImpl.class);
                 LambdaQueryWrapper<BpmBusinessProcess> queryWrapper = Wrappers.<BpmBusinessProcess>lambdaQuery()
                         .eq(BpmBusinessProcess::getBusinessId, idValue)
-                        .eq(BpmBusinessProcess::getIsLowCodeFlow, 0);
+                        .eq(BpmBusinessProcess::getIsLowCodeFlow, 1);
                 BpmBusinessProcess bpmBusinessProcess = businessProcessService.getOne(queryWrapper);
                 if(bpmBusinessProcess!=null){
                     return bpmBusinessProcess.getProcessinessKey();
@@ -194,13 +194,24 @@ public class LFConsistentHashingRoutingSqlInterceptor implements Interceptor, In
 
         } catch (Exception e) {
             throw new RuntimeException(e);
-        }
+        }*/
         for (ParameterMapping parameterMapping : parameterMappings) {
             String propertyName = parameterMapping.getProperty();
+            if("ID".equalsIgnoreCase(propertyName)){
+                //虽然理论上也也存在where id=xxx and a=x and b=x这种sql,但是默认用户不会这么写,所以暂时不支持
+                    BpmBusinessProcess bpmBusinessProcess = SpringBeanUtils.getBean(BpmBusinessProcessServiceImpl.class).getOne(Wrappers.<BpmBusinessProcess>lambdaQuery()
+                            .eq(BpmBusinessProcess::getBusinessId, parameterObject)
+                            .eq(BpmBusinessProcess::getIsLowCodeFlow, 1));
+                    if(bpmBusinessProcess!=null){
+                        return bpmBusinessProcess.getProcessinessKey();
+                    }else{
+                        throw new JiMuBizException("无法根据低代码流程指定Id:"+parameterObject+"找到流程信息,对应的流程不存在!");
+                    }
+            }
             if(!FORM_CODES_UPPER.contains(propertyName.toUpperCase())){
                 continue;
             }
-            Object value;
+            Object value=null;
 
 
             if (boundSql.hasAdditionalParameter(propertyName)) {
