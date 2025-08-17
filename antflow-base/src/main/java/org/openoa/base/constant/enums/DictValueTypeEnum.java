15a91151dc46ee3d067d4ac5886ced3f53be6d9e
TylerZhou <zypqqgc@qq.com>
2025-08-17 10:57:05 +0800

    1.字典增加值类型和二级类型,支持更复杂数据存储和分类

---
+++ antflow-base/src/main/java/org/openoa/base/constant/enums/DictValueTypeEnum.java
@@ -0,0 +1,21 @@
+package org.openoa.base.constant.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.openoa.base.vo.BaseIdTranStruVo;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public enum DictValueTypeEnum {
+    STRING(0,"字符串"),
+    NUM(1,"数值"),
+    BASEIDTRANSTRUVO(3,"简单对象"),
+    ARRAY_OF_IDNAMESTRCT(4,"简单对象数组"),
+    ARRAY_OF_STRING(5,"字符串数组"),
+    ARRAY_OF_NUM(6,"数字数组"),
+    ;
+    private Integer code;
+    private String name;
+}
