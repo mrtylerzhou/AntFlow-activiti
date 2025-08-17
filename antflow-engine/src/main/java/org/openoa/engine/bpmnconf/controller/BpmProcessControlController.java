28d3cee5913144b15617b3273982ad23ad1486b3
TylerZhou <zypqqgc@qq.com>
2025-08-17 10:40:04 +0800

    1.去除没有用上的字段,减少用户理解困难

--- antflow-engine/src/main/java/org/openoa/engine/bpmnconf/controller/BpmProcessControlController.java
+++ antflow-engine/src/main/java/org/openoa/engine/bpmnconf/controller/BpmProcessControlController.java
@@ -1,13 +1,16 @@
 package org.openoa.engine.bpmnconf.controller;
 
+import org.openoa.base.constant.enums.NodeFormAssigneePropertyEnum;
 import org.openoa.base.entity.Result;
+import org.openoa.base.vo.BaseIdTranStruVo;
+import org.openoa.base.vo.BaseNumIdStruVo;
 import org.openoa.base.vo.BpmProcessDeptVo;
 import org.openoa.engine.bpmnconf.service.interf.biz.BpmProcessDeptBizService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author: jwz
@@ -32,5 +35,13 @@ public class BpmProcessControlController {
     public Result saveProcessNotices(@RequestBody BpmProcessDeptVo vo) {
         processDeptBizService.editProcessConf(vo);
         return Result.success();
+    }
+    @GetMapping("/getFormRelatedOptions")
+    public Result<List<BaseNumIdStruVo>> getFormRelatedOptions(){
+        List<BaseNumIdStruVo> list=new ArrayList<>();
+        for (NodeFormAssigneePropertyEnum value : NodeFormAssigneePropertyEnum.values()) {
+            list.add(BaseNumIdStruVo.builder().id(value.getCode().longValue()).name(value.getDesc()).build());
+        }
+        return Result.newSuccessResult(list);
     }
 }
