cec132a06445171baec1f0e4dd0ea348286dda11
lidonghui <574427343@qq.com>
2025-08-29 22:02:28 +0800

    版本信息更新

--- antflow-vue/src/views/index.vue
+++ antflow-vue/src/views/index.vue
@@ -2,8 +2,8 @@
   <div class="app-container home">
     <shortcuts />
     <el-divider />
-    <el-row :gutter="20">
-      <el-col :sm="24" :lg="12" style="margin-bottom: 5px;">
+    <el-row :gutter="10">
+      <el-col :xs="24" :sm="24" :md="12" :lg="12">
         <el-card class="update-log">
           <template v-slot:header>
             <div class="clearfix">
@@ -24,21 +24,13 @@
             </p>
             <p>
               <el-row :gutter="5">
-                <el-col :xs="12" :sm="12" :md="12" :lg="4">
+                <el-col :xs="12" :sm="12" :md="8" :lg="8">
                   <el-button type="primary" icon="Cloudy" plain style="width: 100%;"
-                    @click="goTarget('https://gitee.com/ldhnet')">访问码云</el-button>
+                    @click="goTarget('https://gitee.com/tylerzhou')">访问码云</el-button>
                 </el-col>
-                <el-col :xs="12" :sm="12" :md="12" :lg="4">
+                <el-col :xs="12" :sm="12" :md="8" :lg="8">
                   <el-button type="primary" icon="Cloudy" plain style="width: 100%;"
-                    @click="goTarget('https://pcn3ojogrp79.feishu.cn/wiki/VO07wArXCibz5Jkvzejcn8CTn8f')">飞书手册</el-button>
-                </el-col>
-                <el-col :xs="24" :sm="24" :md="24" :lg="8">
-                  <el-button type="success" icon="Cloudy" plain style="width: 100%;"
-                    @click="goTarget('https://gitee.com/tylerzhou/Antflow/blob/master/antflow-vue/public/docs/%E7%B3%BB%E7%BB%9F%E6%93%8D%E4%BD%9C%E6%89%8B%E5%86%8C-%E4%BD%8E%E4%BB%A3%E7%A0%81%E7%89%88%E6%9C%AC.md')">操作手册【低代码表单】</el-button>
-                </el-col>
-                <el-col :xs="24" :sm="24" :md="24" :lg="8">
-                  <el-button type="success" icon="Cloudy" plain style="width: 100%;"
-                    @click="goTarget('https://gitee.com/tylerzhou/Antflow/blob/master/antflow-vue/public/docs/%E7%B3%BB%E7%BB%9F%E6%93%8D%E4%BD%9C%E6%89%8B%E5%86%8C-%E8%87%AA%E5%AE%9A%E4%B9%89%E8%A1%A8%E5%8D%95.md')">操作手册【自定义表单】</el-button>
+                    @click="goTarget('https://gitee.com/ldhnet/AntFlow-Designer')">流程设计器</el-button>
                 </el-col>
               </el-row>
             </p>
@@ -46,7 +38,7 @@
         </el-card>
       </el-col>
 
-      <el-col :xs="24" :sm="24" :md="12" :lg="12" style="margin-bottom: 5px;">
+      <el-col :xs="24" :sm="24" :md="12" :lg="12">
         <el-card class="update-log">
           <template v-slot:header>
             <div class="clearfix">
@@ -66,13 +58,33 @@
               <i class="el-icon-user-solid"></i> 邮箱：
               <a>574427343@qq.com</a>
             </p>
+            <p>
+              <el-row :gutter="5">
+                <el-col :xs="12" :sm="12" :md="8" :lg="8">
+                  <el-button type="primary" icon="Cloudy" plain style="width: 100%;"
+                    @click="goTarget('https://pcn3ojogrp79.feishu.cn/wiki/VO07wArXCibz5Jkvzejcn8CTn8f')">飞书手册</el-button>
+                </el-col>
+                <el-col :xs="12" :sm="12" :md="8" :lg="8">
+                  <el-button type="success" icon="Cloudy" plain style="width: 100%;"
+                    @click="goTarget('https://gitee.com/tylerzhou/Antflow/blob/master/antflow-vue/public/docs/%E7%B3%BB%E7%BB%9F%E6%93%8D%E4%BD%9C%E6%89%8B%E5%86%8C-%E4%BD%8E%E4%BB%A3%E7%A0%81%E7%89%88%E6%9C%AC.md')">操作手册【低代码表单】</el-button>
+                </el-col>
+                <el-col :xs="12" :sm="12" :md="8" :lg="8">
+                  <el-button type="success" icon="Cloudy" plain style="width: 100%;"
+                    @click="goTarget('https://gitee.com/tylerzhou/Antflow/blob/master/antflow-vue/public/docs/%E7%B3%BB%E7%BB%9F%E6%93%8D%E4%BD%9C%E6%89%8B%E5%86%8C-%E8%87%AA%E5%AE%9A%E4%B9%89%E8%A1%A8%E5%8D%95.md')">操作手册【自定义表单】</el-button>
+                </el-col>
+                <el-col :xs="12" :sm="12" :md="8" :lg="8">
+                  <el-button type="success" icon="Cloudy" plain style="width: 100%;"
+                    @click="goTarget('https://gitee.com/tylerzhou/Antflow/tree/master/doc')">日常开发文档</el-button>
+                </el-col>
+              </el-row>
+            </p>
           </div>
         </el-card>
       </el-col>
     </el-row>
     <el-divider />
     <el-row :gutter="20">
-      <el-col :xs="24" :sm="24" :md="12" :lg="8" style="margin-bottom: 5px;">
+      <el-col :xs="24" :sm="24" :md="12" :lg="12">
         <el-card class="update-log">
           <template v-slot:header>
             <div class="clearfix">
@@ -80,57 +92,67 @@
             </div>
           </template>
           <el-collapse accordion>
-            <el-collapse-item title="v9.0.0 - 2025-06-27">
+            <el-collapse-item title="v1.2.0 - 2025-06-27">
+              <ol>
+                <li>加签/减签/变更处理人/作废/撤回等功能上线</li>
+              </ol>
+            </el-collapse-item>
+            <el-collapse-item title="v1.0.0 - 2025-06-27">
+              <ol>
+                <li>正式发布1.0版本，gitee获的star突破1000</li>
+              </ol>
+            </el-collapse-item>
+            <el-collapse-item title="v0.9.0 - 2025-06-27">
               <ol>
                 <li>流程通知模块对接</li>
               </ol>
             </el-collapse-item>
-            <el-collapse-item title="v8.0.0 - 2025-05-01">
+            <el-collapse-item title="v0.8.0 - 2025-05-01">
               <ol>
                 <li>流程设计条件节点实现条件分组</li>
               </ol>
             </el-collapse-item>
-            <el-collapse-item title="v7.0.0 - 2025-03-05">
+            <el-collapse-item title="v0.7.0 - 2025-03-05">
               <ol>
                 <li>实现发起人自选审批人</li>
               </ol>
             </el-collapse-item>
-            <el-collapse-item title="v6.0.0 - 2025-02-15">
+            <el-collapse-item title="v0.6.0 - 2025-02-15">
               <ol>
                 <li>添加并行审批功能</li>
                 <li>代码深度优化</li>
                 <li>关键代码添加注释</li>
               </ol>
             </el-collapse-item>
-            <el-collapse-item title="v5.0.0 - 2025-01-11">
+            <el-collapse-item title="v0.5.0 - 2025-01-11">
               <ol>
                 <li>低代码表单优化</li>
               </ol>
             </el-collapse-item>
-            <el-collapse-item title="v4.0.0 - 2024-12-14">
+            <el-collapse-item title="v0.4.0 - 2024-12-14">
               <ol>
                 <li>低代码表单接入</li>
               </ol>
             </el-collapse-item>
-            <el-collapse-item title="v3.1.0 - 2024-10-17">
+            <el-collapse-item title="v0.3.1 - 2024-10-17">
               <ol>
                 <li>三方业务表单接入</li>
                 <li>三方业务方注册，应用注册</li>
                 <li>三方业务流程设计</li>
               </ol>
             </el-collapse-item>
-            <el-collapse-item title="v3.0.0 - 2024-08-10">
+            <el-collapse-item title="v0.3.0 - 2024-08-10">
               <ol>
                 <li>其他细节优化</li>
               </ol>
             </el-collapse-item>
-            <el-collapse-item title="v2.0.0 - 2024-08-08">
+            <el-collapse-item title="v0.2.0 - 2024-08-08">
               <ol>
                 <li>Mock Data</li>
                 <li>其他细节优化</li>
               </ol>
             </el-collapse-item>
-            <el-collapse-item title="v1.0.0 - 2024-08-01">
+            <el-collapse-item title="v0.1.0 - 2024-08-01">
               <ol>
                 <li>防RuoYi-Vue版前端正式发布</li>
               </ol>
@@ -144,7 +166,9 @@
 
 <script setup name="Index">
 import shortcuts from "@/components/Dashboard/Shortcuts";
-const version = ref('0.9.1')
+import useSettingsStore from '@/store/modules/settings'
+const settingsStore = useSettingsStore()
+const version = settingsStore.version;
 console.log("VITE_APP_ENV=======", import.meta.env.VITE_APP_ENV);
 console.log("VITE_APP_BASE_API=======", import.meta.env.VITE_APP_BASE_API);
 console.log("BASE_URL=======", import.meta.env.BASE_URL);
