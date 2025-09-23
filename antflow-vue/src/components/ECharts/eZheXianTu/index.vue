70f6fa43da72bb949a752b445789e1f375debad8
lidonghui <574427343@qq.com>
2025-09-23 13:47:23 +0800

    新增echarts报表展示

---
+++ antflow-vue/src/components/ECharts/eZheXianTu/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div ref="chartDom" style="height:400px"></div>
+</template>
+<script setup>
+import { ref, onMounted } from 'vue'
+import * as echarts from 'echarts';
+const chartDom = ref(null);
+onMounted(() => {
+    const chart = echarts.init(chartDom.value);
+    chart.setOption({
+        xAxis: { type: 'category' },
+        yAxis: { type: 'value' },
+        series: [{ data: [120, 200, 150], type: 'line' }]
+    });
+    window.addEventListener('resize', () => chart.resize()); // 响应式
+});
+</script>
