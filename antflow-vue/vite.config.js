30ccea1f6b5b28680ee9c799192d6cbc9e58b79d
lidonghui <574427343@qq.com>
2025-10-29 09:54:10 +0800

    项目打包优化：拆分第三方依赖 和 将将 vForm 库单独打包

--- antflow-vue/vite.config.js
+++ antflow-vue/vite.config.js
@@ -41,6 +41,20 @@ export default defineConfig(({ mode, command }) => {
           entryFileNames: "static/js/[name]-[hash].js",
           assetFileNames: "static/[ext]/[name]-[hash].[ext]",
         },
+        manualChunks(id) {
+          // 将 vForm 库单独打包
+          if (id.includes("lib/vForm")) {
+            return "lib-vForm";
+          }
+          // 按一级目录拆分第三方依赖
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
       },
       commonjsOptions: {
         include: /node_modules|lib/, //这里记得把lib目录加进来，否则生产打包会报错！！
