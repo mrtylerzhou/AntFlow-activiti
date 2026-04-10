d4e050b261e0e49496c146fdd346d2092794502a
刘风雷 <498672760@qq.com>
2026-04-10 18:04:38 +0800

    diy流程动态条件分支不生效解决

--- antflow-engine/src/main/java/org/openoa/engine/bpmnconf/adp/processoperation/ResubmitProcessImpl.java
+++ antflow-engine/src/main/java/org/openoa/engine/bpmnconf/adp/processoperation/ResubmitProcessImpl.java
@@ -2,6 +2,9 @@ package org.openoa.engine.bpmnconf.adp.processoperation;
 
 import com.alibaba.fastjson2.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.task.Task;
 import org.apache.commons.lang3.StringUtils;
@@ -9,21 +12,22 @@ import org.openoa.base.constant.StringConstants;
 import org.openoa.base.constant.enums.ProcessOperationEnum;
 import org.openoa.base.constant.enums.ProcessSubmitStateEnum;
 import org.openoa.base.dto.NodeExtraInfoDTO;
+import org.openoa.base.entity.BpmBusinessProcess;
+import org.openoa.base.entity.BpmVerifyInfo;
+import org.openoa.base.exception.AFBizException;
 import org.openoa.base.interf.BpmBusinessProcessService;
 import org.openoa.base.interf.ProcessOperationAdaptor;
+import org.openoa.base.util.SecurityUtils;
+import org.openoa.base.vo.BpmnConfVo;
 import org.openoa.base.vo.BpmnNodeLabelVO;
-import org.openoa.base.entity.BpmVerifyInfo;
+import org.openoa.base.vo.BpmnNodeVo;
+import org.openoa.base.vo.BusinessDataVo;
 import org.openoa.engine.bpmnconf.service.biz.BpmnConfBizServiceImpl;
 import org.openoa.engine.bpmnconf.service.biz.BpmnProcessMigrationServiceImpl;
-import org.openoa.base.exception.AFBizException;
-
-import org.openoa.base.entity.BpmBusinessProcess;
-
-import org.openoa.base.vo.BusinessDataVo;
-import org.openoa.base.util.SecurityUtils;
 import org.openoa.engine.bpmnconf.service.interf.biz.BpmProcessNodeSubmitBizService;
 import org.openoa.engine.bpmnconf.service.interf.biz.BpmVariableSignUpPersonnelBizService;
 import org.openoa.engine.bpmnconf.service.interf.biz.BpmVerifyInfoBizService;
+import org.openoa.engine.bpmnconf.service.interf.biz.BpmnConfBizService;
 import org.openoa.engine.bpmnconf.service.interf.repository.BpmVerifyAttachmentService;
 import org.openoa.engine.factory.FormFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,9 +37,10 @@ import org.springframework.util.ObjectUtils;
 
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-import static org.openoa.base.constant.enums.ProcessSubmitStateEnum.PROCESS_SIGN_UP;
 import static org.openoa.base.constant.enums.ProcessOperationEnum.*;
+import static org.openoa.base.constant.enums.ProcessSubmitStateEnum.PROCESS_SIGN_UP;
 
 /**
  * @Author JimuOffice
@@ -67,6 +72,38 @@ public class ResubmitProcessImpl implements ProcessOperationAdaptor {
     @Autowired
     private BpmnConfBizServiceImpl bpmnConfCommonService;
 
+    @Autowired
+    private BpmnConfBizService bpmnConfBizService;
+
+    private static final LoadingCache<Long, BpmnConfVo> cache = CacheBuilder.newBuilder()
+            .maximumSize(1000) // 最大缓存数量
+            .expireAfterWrite(24, TimeUnit.HOURS) // 写入后24小时后过期
+            .build(new CacheLoader<Long, BpmnConfVo>() {
+                @Override
+                public BpmnConfVo load(Long key) throws Exception {
+                    // 实现自动加载逻辑，例如从数据库加载数据
+                    return new BpmnConfVo();
+                }
+            });
+
+    private BpmnConfVo getBpmnConfVoDetail(Long key) {
+        BpmnConfVo bpmnConfVo = null;
+
+        try {
+            bpmnConfVo = cache.get(key);
+            if(null == bpmnConfVo || null == bpmnConfVo.getId()) {
+                bpmnConfVo = this.bpmnConfBizService.detail(key);
+                if(null != bpmnConfVo && null != bpmnConfVo.getId()) {
+                    cache.put(key, bpmnConfVo);
+                }
+            }
+        } catch (Exception e) {
+            return null;
+        }
+
+        return bpmnConfVo;
+    }
+
     @Override
     public void doProcessButton(BusinessDataVo vo) {
         vo.setStartUserId(SecurityUtils.getLogInEmpIdStr());
@@ -82,7 +119,7 @@ public class ResubmitProcessImpl implements ProcessOperationAdaptor {
         }
         Task task;
         if (!ObjectUtils.isEmpty(vo.getTaskId())) {
-            task = tasks.stream().filter(o -> o.getId().equals(vo.getTaskId())&&o.getAssignee().equals(SecurityUtils.getLogInEmpIdStr())).findFirst().orElse(null);
+            task = tasks.stream().filter(o -> o.getId().equals(vo.getTaskId())).findFirst().orElse(null);
         } else {
             task = tasks.get(0);
             if (StringUtils.isEmpty(task.getAssigneeName())) {
@@ -102,10 +139,30 @@ public class ResubmitProcessImpl implements ProcessOperationAdaptor {
                     if (StringConstants.DYNAMIC_CONDITION_NODE.equals(nodeLabelVO.getLabelValue())) {
                         if (tasks.size() == 1) {//只有当前节点到最后一个审批人了才执行迁移
                             boolean conditionsChanged = bpmnConfCommonService.migrationCheckConditionsChange(vo);
-                           if(conditionsChanged){
-                               bpmnProcessMigrationService.migrateAndJumpToCurrent(task, bpmBusinessProcess, vo, this::executeTaskCompletion);
-                               return;
-                           }
+                            if(conditionsChanged){
+                                bpmnProcessMigrationService.migrateAndJumpToCurrent(task, bpmBusinessProcess, vo, this::executeTaskCompletion);
+                                bpmVerifyInfoSupplement(vo, task, bpmBusinessProcess);
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            //diy流程是否含有动态条件判断
+            if(null != vo.getBpmnConfVo() && null != vo.getBpmnConfVo().getId()) {
+                BpmnConfVo bpmnConfVo = this.getBpmnConfVoDetail(vo.getBpmnConfVo().getId());
+                if(null != bpmnConfVo && null != bpmnConfVo.getNodes()) {
+                    for (BpmnNodeVo nodeVo : bpmnConfVo.getNodes()) {
+                        if(nodeVo.getIsDynamicCondition()) {
+                            if (tasks.size() == 1) {//只有当前节点到最后一个审批人了才执行迁移
+                                boolean conditionsChanged = bpmnConfCommonService.migrationCheckConditionsChange(vo);
+                                if(conditionsChanged){
+                                    bpmnProcessMigrationService.migrateAndJumpToCurrent(task, bpmBusinessProcess, vo, this::executeTaskCompletion);
+                                    bpmVerifyInfoSupplement(vo, task, bpmBusinessProcess);
+                                    return;
+                                }
+                            }
                         }
                     }
                 }
@@ -120,12 +177,45 @@ public class ResubmitProcessImpl implements ProcessOperationAdaptor {
         vo.setStartUserId(bpmBusinessProcess.getCreateUser());//这里主要是为了发消息通知使用
     }
 
+    /**
+     * 操作记录补充
+     * @param vo
+     * @param task
+     * @param bpmBusinessProcess
+     */
+    private void bpmVerifyInfoSupplement(BusinessDataVo vo, Task task, BpmBusinessProcess bpmBusinessProcess) {
+        //save process verify info
+        BpmVerifyInfo bpmVerifyInfo = BpmVerifyInfo
+                .builder()
+                .verifyDate(new Date())
+                .taskName(task.getName())
+                .taskId(task.getId())
+                .runInfoId(bpmBusinessProcess.getProcInstId())
+                .verifyUserId(task.getAssignee())
+                .verifyUserName(vo.getStartUserName())
+                .taskDefKey(task.getTaskDefinitionKey())
+                .verifyStatus(ProcessSubmitStateEnum.PROCESS_AGRESS_TYPE.getCode())
+                .verifyDesc(ObjectUtils.isEmpty(vo.getApprovalComment()) ? "同意" : vo.getApprovalComment())
+                .processCode(vo.getProcessNumber())
+                .build();
+
+
+        if (vo.getOperationType().intValue() == BUTTON_TYPE_JP.getCode().intValue()) {
+            bpmVerifyInfo.setVerifyStatus(PROCESS_SIGN_UP.getCode());
+            bpmVerifyInfo.setVerifyDesc(ObjectUtils.isEmpty(vo.getApprovalComment()) ? "加批" : vo.getApprovalComment());
+        }
+
+        if(!StringConstants.CURRENT_USER_ALREADY_PROCESSED.equals(bpmVerifyInfo.getVerifyDesc())){
+            bpmVerifyInfoBizService.addVerifyInfo(bpmVerifyInfo);
+        }
+    }
+
     private void executeTaskCompletion(BusinessDataVo vo, Task task, BpmBusinessProcess bpmBusinessProcess) {
         vo.setTaskId(task.getId());
 //        BusinessDataVo businessDataVo = formFactory.getFormAdaptor(vo).consentData(vo);
 
         if (!vo.getIsOutSideAccessProc()) {
-             formFactory.getFormAdaptor(vo).consentData(vo);
+            formFactory.getFormAdaptor(vo).consentData(vo);
         }
 
         //save process verify info
