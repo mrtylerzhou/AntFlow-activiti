4c374f8624a426356acb3de256fca57634172920
lidonghui <574427343@qq.com>
2026-04-24 22:00:27 +0800

    移除多余组件

--- antflow-vue/src/router/index.js
+++ antflow-vue/src/router/index.js
@@ -95,20 +95,6 @@ export const constantRoutes = [
 // 动态路由，基于用户权限动态去加载
 export const dynamicRoutes = [
   {
-    path: "/system/user-auth",
-    component: Layout,
-    hidden: true,
-    permissions: ["system:user:edit"],
-    children: [
-      {
-        path: "role/:userId(\\d+)",
-        component: () => import("@/views/system/user/authRole"),
-        name: "AuthRole",
-        meta: { title: "分配角色", activeMenu: "/system/user" },
-      },
-    ],
-  },
-  {
     path: "/startFlow/index",
     component: Layout,
     hidden: true,
