4b567406c232f371afb3f8f165e38a5f620d1b88
lidonghui <574427343@qq.com>
2026-06-17 21:02:12 +0800

    code review

--- antflow-vue/src/components/Workflow/DynamicForm/index.vue
+++ antflow-vue/src/components/Workflow/DynamicForm/index.vue
@@ -24,12 +24,12 @@ let formField = {};
 //let formImportObj = "{\"widgetList\":[{\"key\":95565,\"type\":\"input\",\"icon\":\"text-field\",\"formItemFlag\":true,\"options\":{\"name\":\"input57337\",\"label\":\"部门\",\"labelAlign\":\"\",\"type\":\"text\",\"defaultValue\":\"\",\"placeholder\":\"\",\"columnWidth\":\"200px\",\"size\":\"\",\"labelWidth\":null,\"labelHidden\":false,\"readonly\":false,\"disabled\":false,\"hidden\":false,\"clearable\":true,\"showPassword\":false,\"required\":false,\"requiredHint\":\"\",\"validation\":\"\",\"validationHint\":\"\",\"customClass\":[],\"labelIconClass\":null,\"labelIconPosition\":\"rear\",\"labelTooltip\":null,\"minLength\":null,\"maxLength\":null,\"showWordLimit\":false,\"prefixIcon\":\"\",\"suffixIcon\":\"\",\"appendButton\":false,\"appendButtonDisabled\":false,\"buttonIcon\":\"custom-search\",\"onCreated\":\"\",\"onMounted\":\"\",\"onInput\":\"\",\"onChange\":\"\",\"onFocus\":\"\",\"onBlur\":\"\",\"onValidate\":\"\",\"onAppendButtonClick\":\"\",\"fieldTypeName\":\"input\",\"fieldType\":\"1\"},\"id\":\"input57337\"}],\"formConfig\":{\"modelName\":\"formData\",\"refName\":\"vForm\",\"rulesName\":\"rules\",\"labelWidth\":80,\"labelPosition\":\"left\",\"size\":\"\",\"labelAlign\":\"label-left-align\",\"cssCode\":\"\",\"customClass\":[],\"functions\":\"\",\"layoutType\":\"PC\",\"jsonVersion\":3,\"onFormCreated\":\"\",\"onFormMounted\":\"\",\"onFormDataChange\":\"\"}}";
 const observer = new MutationObserver(() => {
   const returnFiled = formDesign.value.getFormFieldJson();
-  if (isObjectChanged(formField, returnFiled)) {
-    formField = returnFiled;
+  const formatReturnField = autoAddFieldType(returnFiled);
+  if (isObjectChanged(formField, formatReturnField)) {
+    formField = formatReturnField;
     store.setLowCodeFormField(formField);
   }
 });
-
 onMounted(() => {
   const targetNode = document.querySelector('#designer-id');
   const config = { childList: true, subtree: true };
@@ -67,6 +67,54 @@ const getFieldList = () => {
     resolve({ formData: exportField.formFields })
     reject(new Error('获取表单获取字段失败'));
   })
+}
+
+/**
+ * 给json字符串中指定key的widget的options添加字段
+ * @param {string} jsonStr - 原始JSON字符串 
+ * @returns {string} 处理后的新JSON字符串
+ */
+function autoAddFieldType(data) {
+  try {
+    if (Array.isArray(data.formFields)) {
+      data.formFields.forEach(widget => {
+        // 根据type获取对应fieldType数值
+        const fieldType = getFieldTypeByType(widget.type);
+        // 确保options对象存在
+        if (!widget.options) widget.options = {};
+        widget.options.fieldType = fieldType;
+      });
+    }
+
+    return data;
+  } catch (err) {
+    return data;
+  }
+}
+
+/**
+ * 根据type映射fieldType数字
+ * @param {string} type widget的type字段
+ * @returns {number} fieldType值
+ */
+function getFieldTypeByType(type) {
+  switch (type) {
+    case "number":
+      return 2;//数字
+    case "date":
+      return 4; //日期时间
+    case "switch":
+      return 6;//boolean
+    case "rich-editor":
+      return 5;//text
+    case "select":
+    case "input":
+    case "checkbox":
+    case "time":
+      return 1;//string
+    default:
+      return 1;
+  }
 }
 defineExpose({
   getData,
