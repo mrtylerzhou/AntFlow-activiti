faeeb830a1fda2abea0e91f3f70e73b215caee05
TylerZhou <zypqqgc@qq.com>
2026-07-04 19:54:54 +0800

    修复流程消息发送人员上下文信息丢失bug

--- antflow-engine/src/main/java/org/openoa/engine/bpmnconf/service/biz/BpmVariableMessageBizServiceImpl.java
+++ antflow-engine/src/main/java/org/openoa/engine/bpmnconf/service/biz/BpmVariableMessageBizServiceImpl.java
@@ -632,9 +632,6 @@ public class BpmVariableMessageBizServiceImpl implements BpmVariableMessageBizSe
             sendUsers.addAll(new ArrayList<>(bpmnTemplateVo.getEmpIdList()));
         }
 
-        if(!StringUtils.isEmpty(vo.getAssignee())&&EventTypeEnum.PROCESS_FLOW.getCode().equals(vo.getEventType())){
-            sendUsers.add(vo.getAssignee());
-        }
 
         //specified roles
         if (!CollectionUtils.isEmpty(bpmnTemplateVo.getRoleIdList())) {
